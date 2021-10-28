f72c515f6bef197f94e5f2ae0b8e0cdb6be483d1
MrPink <rubberandrobber@gmail.com>
2021-10-28 11:10:44 +0300

    camera

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -18,8 +18,10 @@ public class CameraController : MonoBehaviour
     private Quaternion targetRotation;
     private bool canFollow = false;
     
-    private float mouseInputY = 0;
-    private float mouseInputZ = 0;
+    private float camParentTargetX = 0;
+    private float camParentTargetY = 0;
+    private float camTargetZ = 0;
+    private Vector3 targetPosition;
     private IEnumerator Start()
     {
         while (PlayerInput.Instance == null && PartyInputManager.Instance == null)
@@ -27,18 +29,19 @@ public class CameraController : MonoBehaviour
             yield return null;
         }
 
+        camParentTargetX = parent.transform.localPosition.x;
+        camParentTargetY = parent.transform.localPosition.y;
+        camTargetZ = transform.localPosition.z;
         yield return new WaitForSeconds(1);
         canFollow = true;
     }
 
-
-    void LateUpdate()
+    void Update()
     {
-        if (canFollow == false)
-            return;
+        // input & calculations
         
-        // parent smoothly follows it
-        Vector3 targetPosition = parent.gameObject.transform.position;
+        targetPosition = parent.gameObject.transform.position;
+        
         if (PlayerInput.Instance)
             targetPosition = PlayerInput.Instance.transform.position + PlayerInput.Instance.transform.forward * distanceInFrontOfCharacter;
         else if (PartyInputManager.Instance)
@@ -57,40 +60,27 @@ public class CameraController : MonoBehaviour
             
             Vector3 movementVector = forward * verticalAxis + right * horizontalAxis;
             targetPosition += movementVector.normalized * (cameraMoveSpeed * Time.deltaTime);
+                
+            camTargetZ += Input.GetAxis("Mouse ScrollWheel") * cameraZoomSpeed * Time.deltaTime;
+            camTargetZ = Mathf.Clamp(camTargetZ, -60f, -10f);
+            
+            if (Input.GetButton("Aim"))
+            {
+                camParentTargetX -= Input.GetAxis("Mouse Y") * cameraTurnSpeed * Time.deltaTime;
+                camParentTargetX = Mathf.Clamp (camParentTargetX, xMinMax.x, xMinMax.y);
+                camParentTargetY += Input.GetAxis("Mouse X") * cameraTurnSpeed * Time.deltaTime;
+            }
         }
+    }
+
+    void LateUpdate()
+    {
+        if (canFollow == false)
+            return;
         
         parent.gameObject.transform.position = Vector3.Lerp(parent.gameObject.transform.position, targetPosition, cameraSmooth * Time.deltaTime);
-
-        /*
-        mouseInputZ = Input.GetAxis("Mouse ScrollWheel");
-        Vector3 posTemp = transform.position + transform.forward.normalized * (mouseInputZ * cameraZoomSpeed * Time.deltaTime);
-        posTemp = new Vector3(posTemp.x, Mathf.Clamp(posTemp.y, 10, 40), posTemp.z);
-        transform.position = posTemp;
-
-        float newCameraTurnSpeed = cameraTurnSpeed * Mathf.InverseLerp(10, 40,posTemp.y);
-        
-        if (Input.GetButton("Aim"))
-        {
-            parent.transform.Rotate(new Vector3(0, Input.GetAxis("Mouse X"), 0) * (newCameraTurnSpeed * Time.deltaTime));
-
-            mouseInputY = -Input.GetAxis("Mouse Y");
-            
-            if (transform.localEulerAngles.x > xMinMax.y && mouseInputY > 0)
-            {
-                mouseInputY = 0;   
-            }
-            else if (transform.localEulerAngles.x < xMinMax.x && mouseInputY < 0)
-                mouseInputY = 0;
-        
-            var resultEulerAngles = parent.transform.eulerAngles;
-            resultEulerAngles += new Vector3(mouseInputY, 0, 0) * ((newCameraTurnSpeed / 5) * Time.deltaTime);
-            parent.transform.eulerAngles = resultEulerAngles;
-            
-            //parent.transform.Rotate(new Vector3(mouseInputY, 0, 0) * ((newCameraTurnSpeed / 5) * Time.deltaTime));
-   
-            /*
-            var resultEulerAngles = parent.transform.eulerAngles;
-            parent.transform.eulerAngles = new Vector3(Mathf.Clamp(resultEulerAngles.x,xMinMax.x,xMinMax.y), parent.transform.eulerAngles.y, parent.transform.eulerAngles.z);#1#
-        }*/
+        parent.transform.eulerAngles = new Vector3 (camParentTargetX, camParentTargetY, 0.0f);
+        // zoom
+        transform.localPosition = new Vector3(0, 0, camTargetZ);
     }
 }
