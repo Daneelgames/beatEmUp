99dd57f0f6c98871c42329818e0b15db6343755e
MrPink <rubberandrobber@gmail.com>
2021-10-28 16:56:59 +0300

    5

---
+++ Assets/Editor/FieldOfViewEditor.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using UnityEngine.Rendering.VirtualTexturing;
+using UnityEngine.XR;
+
+[CustomEditor(typeof(FieldOfView))]
+public class FieldOfViewEditor : Editor
+{
+    void OnSceneGUI()
+    {
+        FieldOfView fow = (FieldOfView)target;
+        Handles.color = Color.white;
+        Handles.DrawWireArc(fow.transform.position, Vector3.up, Vector3.forward, 360, fow.ViewRadius);
+        Vector3 viewAngleA = fow.DirFromAngle(-fow.ViewAngle / 2, false);
+        Vector3 viewAngleB = fow.DirFromAngle(fow.ViewAngle / 2, false);
+        
+        Handles.DrawLine(fow.transform.position, fow.transform.position + viewAngleA * fow.ViewRadius);
+        Handles.DrawLine(fow.transform.position, fow.transform.position + viewAngleB * fow.ViewRadius);
+
+        Handles.color = Color.red;
+        
+        foreach (Transform visibleTargets in fow.VisibleTargets)
+        {
+            Handles.DrawLine(fow.transform.position, visibleTargets.position);
+        }
+    }
+}
